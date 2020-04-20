6a01c13c926e532de72b9f80ccfbe5cddfc3ef7b
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-19 19:53:43 -0500

    create JohnSnow.js

---
+++ src/components/JohnSnow.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+
+
+class JohnSnow extends React.Component {
+    render() {
+        return(
+            <div>
+                    apple
+            </div>
+
+
+        )
+    }
+
+}
+
+export default JohnSnow;
