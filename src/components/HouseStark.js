c9ab1a9eddd28547a94c354adc46a99c07f72ef3
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-29 21:20:33 -0500

    update readme file

--- src/components/HouseStark.js
+++ src/components/HouseStark.js
@@ -5,29 +5,33 @@ class HouseStark extends React.Component {
     constructor(props){
         super(props);
         this.state={ 
-            bizOwner: " "
+            bizOwner: " ",
+             founderGot: " "
          }
         }
         componentDidMount() {
             axios.get("https://www.anapioficeandfire.com/api/houses/362")
             .then(res => {
-                this.setState({bizOwner: res.data.founder}) 
-               console.log(this.state.bizOwner)
-    
-            }).catch(error=>{
-                cath(error);
+                this.setState({bizOwner:res.data.founder})
+                console.log(this.state.bizOwner)
+                axios.get(this.state.bizOwner)
+                .then(result => {
+                    this.setState({founderGot: result.data.name}) 
             })
-
+            }).catch(error=>{
+               console.log(error);
+            })
+    
         }
+    
         render() {
         return(
             <div>
                 <h1>Q6: What's the name of the founder of House Stark? (You have to chain fetch requests!)</h1>
-                <h1>A6: the name of the founder of House Stark is: {this.state.bizOwner}</h1>   
+                <h1>A6: the name of the founder of House Stark is: {this.state.founderGot}</h1>   
             </div>
         )
     }
-
 }
-
 export default HouseStark;
+
